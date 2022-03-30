864bc2fc0a98d6b6fe95116a323e3aeea1d7c70a
MinervaArgus <71613767+MinervaArgus@users.noreply.github.com>
2022-03-30 17:33:42 -0400

    Added Google Sign in

--- server/index.js
+++ server/index.js
@@ -1,3 +1,7 @@
+import { FirebaseSignInProvider } from "@firebase/util";
+import firebase from "firebase";
+import firebaseConfig from "./firebaseConfig";
+
 function toggleSignup(){
     document.getElementById("login-toggle").style.backgroundColor="#fff";
      document.getElementById("login-toggle").style.color="#222";
@@ -28,3 +32,26 @@ function toggleSignup(){
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+document.addEventListener('DOMContentLoaded', event => {
+    const app = firebase.app();
+});
+
+function googleLogin() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+}
