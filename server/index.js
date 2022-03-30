5481bb0db697615d39c5bfcc45d29f4d57a2de81
MinervaArgus <71613767+MinervaArgus@users.noreply.github.com>
2022-03-30 18:28:02 -0400

    Lots of Troubleshooting and package correction

--- server/index.js
+++ server/index.js
@@ -1,57 +1,73 @@
-import { FirebaseSignInProvider } from "@firebase/util";
-import firebase from "firebase";
-import firebaseConfig from "./firebaseConfig";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import {
+  auth,
+  signInWithRedirect,
+  GoogleAuthProvider,
+  getRedirectResult,
+} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
 
-function toggleSignup(){
-    document.getElementById("login-toggle").style.backgroundColor="#fff";
-     document.getElementById("login-toggle").style.color="#222";
-     document.getElementById("signup-toggle").style.backgroundColor="#57b846";
-     document.getElementById("signup-toggle").style.color="#fff";
-     document.getElementById("login-form").style.display="none";
-     document.getElementById("signup-form").style.display="block";
- }
- 
- function toggleLogin(){
-     document.getElementById("login-toggle").style.backgroundColor="#57B846";
-     document.getElementById("login-toggle").style.color="#fff";
-     document.getElementById("signup-toggle").style.backgroundColor="#fff";
-     document.getElementById("signup-toggle").style.color="#222";
-     document.getElementById("signup-form").style.display="none";
-     document.getElementById("login-form").style.display="block";
- }
+var loginFormGLogin = document.getElementById("loginFormGLogin");
+var signupFormGLogin = document.getElementById("signupFormGLogin");
+// TODO: Add SDKs for Firebase products that you want to use
+//   https://firebase.google.com/docs/web/setup#available-libraries
 
- const firebaseConfig = {
-    apiKey: "AIzaSyBbdXlLKxXF4_j94XhnnjHJZKW0x5Hvv80",
-    authDomain: "ohayo-d5749.firebaseapp.com",
-    projectId: "ohayo-d5749",
-    storageBucket: "ohayo-d5749.appspot.com",
-    messagingSenderId: "409271309698",
-    appId: "1:409271309698:web:95e19acfeacd23659aeb9b",
-    measurementId: "G-K8S225CMZ3"
-  };
+//   Your web app's Firebase configuration
+//   For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBbdXlLKxXF4_j94XhnnjHJZKW0x5Hvv80",
+  authDomain: "ohayo-d5749.firebaseapp.com",
+  projectId: "ohayo-d5749",
+  storageBucket: "ohayo-d5749.appspot.com",
+  messagingSenderId: "409271309698",
+  appId: "1:409271309698:web:95e19acfeacd23659aeb9b",
+  measurementId: "G-K8S225CMZ3",
+};
 
+//   Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-document.addEventListener('DOMContentLoaded', event => {
-    const app = firebase.app();
-});
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider(app);
 
-function googleLogin() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
+function loginFormSignInWithGoogle() {
+  loginFormGLogin.addEventListener("click", (event) => {
+    signInWithRedirect(auth, provider);
+
+    getRedirectResult(auth)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access Google APIs.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+
         // The signed-in user info.
-        var user = result.user;
-        // ...
-    }).catch(function(error) {
+        const user = result.user;
+      })
+      .catch((error) => {
         // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
+        const errorCode = error.code;
+        const errorMessage = error.message;
         // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
         // ...
-    });
+      });
+  });
+}
+
+function toggleSignup() {
+  document.getElementById("login-toggle").style.backgroundColor = "#fff";
+  document.getElementById("login-toggle").style.color = "#222";
+  document.getElementById("signup-toggle").style.backgroundColor = "#57b846";
+  document.getElementById("signup-toggle").style.color = "#fff";
+  document.getElementById("login-form").style.display = "none";
+  document.getElementById("signup-form").style.display = "block";
+}
+
+function toggleLogin() {
+  document.getElementById("login-toggle").style.backgroundColor = "#57B846";
+  document.getElementById("login-toggle").style.color = "#fff";
+  document.getElementById("signup-toggle").style.backgroundColor = "#fff";
+  document.getElementById("signup-toggle").style.color = "#222";
+  document.getElementById("signup-form").style.display = "none";
+  document.getElementById("login-form").style.display = "block";
 }
