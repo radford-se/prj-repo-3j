8b4832f1884e9cd9d28e482ebbd4c94dabdc634e
MinervaArgus <71613767+MinervaArgus@users.noreply.github.com>
2022-03-30 13:46:13 -0400

    Added Esentials for Firebase

--- server/index.js
+++ server/index.js
@@ -15,3 +15,16 @@ function toggleSignup(){
      document.getElementById("signup-form").style.display="none";
      document.getElementById("login-form").style.display="block";
  }
+
+ const firebaseConfig = {
+    apiKey: "AIzaSyBbdXlLKxXF4_j94XhnnjHJZKW0x5Hvv80",
+    authDomain: "ohayo-d5749.firebaseapp.com",
+    projectId: "ohayo-d5749",
+    storageBucket: "ohayo-d5749.appspot.com",
+    messagingSenderId: "409271309698",
+    appId: "1:409271309698:web:95e19acfeacd23659aeb9b",
+    measurementId: "G-K8S225CMZ3"
+  };
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
