811d9fd28cb805d48c1f276026a6a90059f0e2f4
MinervaArgus <71613767+MinervaArgus@users.noreply.github.com>
2022-03-30 21:54:17 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,21 +1,21 @@
+var firebaseauth = require('firebaseauth');
 var firebase = require('firebase');
-var firebaseui = require('firebaseui'); 
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
+var firebaseui
+function emailLogin(){
+  
 
-ui.start('#firebaseui-auth-container', {
-  signInOptions: [
-    firebase.auth.EmailAuthProvider.PROVIDER_ID
-  ],
-  // Other config options...
-});
-ui.start('#firebaseui-auth-container', {
-  signInOptions: [
-    // List of OAuth providers supported.
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-  ],
-  // Other config options...
-});
-ui.start('#firebaseui-auth-container', uiConfig);
+const auth = getAuth();
+signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+} 
 
 function toggleSignup(){
   document.getElementById("login-toggle").style.backgroundColor="#fff";
