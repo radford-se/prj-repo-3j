b37712a36fb049cf96db84d28fd7010dcec5fb68
joebryan777 <jhbryant1@radford.edu>
2022-04-30 17:31:50 -0400

    state for weather

--- client/public/weather.js
+++ client/public/weather.js
@@ -1,11 +1,10 @@
 let measurement = "metric";
 let weather = {
   apiKey: "f2c6e85d6afc687fda1421a8b419e1f4",
-  fetchWeather: function (city, measure) {
+  fetchWeather: function (lat,lon,measure) {
     fetch(
 
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-      city +
+      "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon +
       "&units=" + measure + "&appid=" +
       this.apiKey
 
@@ -20,12 +19,49 @@ let weather = {
       })
       .then((data) => this.displayWeather(data));
   },
+  fetchCords: function (city,state) {
+    fetch(
+
+
+
+      "http://api.openweathermap.org/geo/1.0/direct?q=" + city + ",US&limit=11&appid=" + this.apiKey
+
+
+    )
+      .then((response) => {
+        if (!response.ok) {
+          alert("No weather found.");
+          throw new Error("No weather found.");
+        }
+        return response.json();
+      })
+      .then((data) => this.getCords(data,state));
+  },
+
+  getCords: function (data,astate) {
+    const { lat } = null;
+    const { lon } = null;
+    for (var i=0; i < data.length; i++){
+
+      if (data.state == astate) {
+
+        lat = data[i];
+        lon = data[i];
+        
+
+      }
+
+    }
+
+    this.fetchWeather(lat,lon,measurement);
+   
+  },
+  
   displayWeather: function (data) {
-    const { name } = data;
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
-    document.querySelector(".city").innerText = name;
+    document.querySelector(".city").innerText = document.querySelector(".search-bar").value;
     document.querySelector(".icon").src =
       "https://openweathermap.org/img/wn/" + icon + ".png";
     document.querySelector(".description").innerText = description;
@@ -47,7 +83,7 @@ let weather = {
     document.querySelector(".weather").classList.remove("loading");
   },
   search: function () {
-    this.fetchWeather(document.querySelector(".search-bar").value, measurement);
+    this.fetchCords(document.querySelector(".search-bar").value, document.querySelector(".state").value);
   },
 };
 
@@ -56,12 +92,12 @@ document.querySelector(".search button").addEventListener("click", function () {
 });
 
 document.querySelector("#imperial").addEventListener("click", function () {
-  measurement = measurement.replace("metric","imperial");
+  measurement = measurement.replace("metric", "imperial");
   weather.fetchWeather(document.querySelector(".city").innerText, measurement);
 });
 
 document.querySelector("#metric").addEventListener("click", function () {
-  measurement = measurement.replace("imperial","metric");
+  measurement = measurement.replace("imperial", "metric");
   weather.fetchWeather(document.querySelector(".city").innerText, measurement);
 });
 
@@ -73,4 +109,4 @@ document
     }
   });
 
-weather.fetchWeather("Radford", measurement);
+weather.fetchWeather("37.1381984","-79.6178087", measurement);
